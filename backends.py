a852126511acc1865fcf1ec2eb66a944f655c146
Federico Gentile <gentfede+git@gmail.com>
2020-07-18 22:32:46 +0200

    chore: add  category to feduquizdb

--- backends.py
+++ backends.py
@@ -17,6 +17,6 @@ backends = {
         "name": "Feduquiz DB",
         "url": "https://dillendapp.eu/feduquizdb/api/trivia",
         "token": "https://dillendapp.eu/feduquizdb/api/token/request",
-        "categories": [["All", -1], ["General knowledge", 1],["Luxemburgensia", 2],["Cooking & baking", 3],["Italianità", 4]]
+        "categories": [["All", -1], ["General knowledge", 1],["Luxemburgensia", 2],["Cooking & baking", 3],["Italianità", 4],["Die bucklige Verwandtschaft",9]]
     }
 }
